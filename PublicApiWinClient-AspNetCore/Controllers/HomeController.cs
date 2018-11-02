077df0d6ee0f8970bb3a318371c95a6fde9ac535
CristianF72 <cristian.frent@gmail.com>
2018-11-02 16:52:23 +0200

    get json and map to object, display values from it

--- PublicApiWinClient-AspNetCore/Controllers/HomeController.cs
+++ PublicApiWinClient-AspNetCore/Controllers/HomeController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using Newtonsoft.Json;
 using PublicApi_AspNetCore_Client.Models;
 
 namespace PublicApi_AspNetCore_Client.Controllers
@@ -16,18 +19,30 @@ namespace PublicApi_AspNetCore_Client.Controllers
         }
         public ActionResult CurrencyConversion()
         {
+            Currency currency = (Currency)GetCurrency();
+
+            ViewData["myModel"] = currency;
+
             return View();
         }
 
-        public JsonResult GetCurrency()
+        public object GetCurrency()
         {
-            Currency currency = new Currency();
-            return Json(currency.getCurrencyFromFixerIo(),JsonRequestBehavior.AllowGet);
-        }
+            string appid = "82317600e69a22e0938a30092fbfa178";
+            string url = "http://data.fixer.io/api/latest?access_key=" + appid;
+            //synchronous client.
+            var client = new WebClient();
+            var content = client.DownloadString(url);
+            //var serializer = new JavaScriptSerializer();
+            var jsonContent = JsonConvert.DeserializeObject<Currency>(content);
+            return jsonContent;
 
-        public void GetConversion()
+            
+        }
+        [HttpPost]
+        public ActionResult CurrencyConversion(decimal valueForConversion)
         {
-           // string fromCurrency = 
+            //return Content(${valueForConversion})
         }
 
     }
--- PublicApiWinClient-AspNetCore/Models/Currency.cs
+++ PublicApiWinClient-AspNetCore/Models/Currency.cs
@@ -2,25 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web.Mvc;
 using System.Web.Script.Serialization;
+using Newtonsoft.Json;
+using PublicApi_AspNetCore_Client.Controllers;
 
 namespace PublicApi_AspNetCore_Client.Models
 {
-    class Currency
+    public class Currency
     {
-        public Object getCurrencyFromFixerIo()
-        {
-            string appid = "82317600e69a22e0938a30092fbfa178";
-            string url = "http://data.fixer.io/api/latest?access_key=" + appid;
-            //synchronous client.
-            var client = new WebClient();
-            var content = client.DownloadString(url);
-            var serializer = new JavaScriptSerializer();
-            var jsonContent = serializer.Deserialize<Object>(content);
-            return jsonContent;
-        }
+        public string Base { get; set; }
+        public DateTime Date { get; set; }
+        public Dictionary<string, decimal> Rates { get; set; }
     }
 }
